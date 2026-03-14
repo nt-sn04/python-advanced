b060cf0be56dca901eb732c83df7998181751f9d
djumanov <djumanovdev@gmail.com>
2026-03-14 19:25:11 +0500

    decorator

--- decorators.py
+++ decorators.py
@@ -1,37 +1,22 @@
-products = []
 
-user01 = {
-    'username': 'ali',
-    'role': 'admin'
-}
-user02 = {
-    'username': 'vali',
-    'role': 'user'
-}
-product = {
-    'name': 'samsung s22',
-    'price': 250
-}
-
-
-def is_admin(func):
-    
-    def wrapper(user: dict, *args, **kwargs):
-        
-        if user['role'] == 'admin':
-            data = func(user=user, *args, **kwargs)
-
-            return data
-        else:
-            raise Exception('siz admin emassiz.')
-
+def bold(func):
+    def wrapper(text: str):
+        text = f'<b>{text}</b>'
+        return func(text)
     return wrapper
 
 
-@is_admin
-def create_product(user: dict, product: dict):
-    products.append(product)
+def italic(func):
+    def wrapper(text: str):
+        text = f'<i>{text}</i>'
+        return func(text)
+    return wrapper
 
-create_product(user=user01, product=product)
-create_product(user=user02, product=product)
-print(products)
+
+@italic
+@bold
+def matn(text: str) -> str:
+    return text
+
+text = matn('salom')
+print(text)
